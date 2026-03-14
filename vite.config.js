c1cbc89b7cd30e05047a9de8ca15821799e878b8
Ronan245 <C1585-23@itmlyceumalabang.onmicrosoft.com>
2026-03-14 19:36:06 +0800

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/PF_P2_Portfolio_BSIT32E2_GA_RONAN-JUSTINE/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
 })
